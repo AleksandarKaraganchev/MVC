564e4c0b30bdd27685a276597e188046e24b0c0f
1212-6 <1212-6@DESKTOP-S2GN1SG>
2024-10-08 13:01:16 +0300

    Add project files.

---
+++ MyFirstMVC/Views/Display.cs
@@ -0,0 +1,31 @@
+﻿using MyFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Views
+{
+    public class Display
+    {
+        public double Amount { get; set; }
+        public double Percent { get; set; }
+        public double TipSum { get; set; }
+        public double TotalSum { get; set; }
+        public void Input()
+        {
+            Console.Write("Enter the amount of the check: ");
+            double amount = double.Parse(Console.ReadLine());
+            this.Amount = amount;
+            Console.Write("Enter tip in percentages: ");
+            double percent = double.Parse(Console.ReadLine());
+            this.Percent = percent;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"The tip of for the waiter is {this.TipSum}lv");
+            Console.WriteLine($"The check is {this.TotalSum}lv");
+        }
+    }
+}
